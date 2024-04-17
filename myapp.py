a7fe016e4582e455a0ba7f3f426218c381f4e413
Larry Rios <lrios@redhat.com>
2024-04-17 14:24:16 -0500

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -73,7 +73,7 @@ def get_request(endpoint):
     """
     print(request.headers)
     app.logger.info(f"printing header of request: {request.headers} ")
-    app.logger.info(f"testing get json { request.get_json() }")
+    #app.logger.info(f"testing get json { request.get_json() }")
     # message_id = request.get_json()["events"][0]["metadata"]["message_id"]
     #message_id = request.text
     #print(f"testing {message_id} ")
